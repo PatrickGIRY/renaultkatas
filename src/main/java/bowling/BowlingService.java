f527f3ef11a9b2c6fa77a60f5de407c4eed8c42c
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2019-10-24 17:26:42 +0200

    manage dynamic strike

--- src/main/java/bowling/BowlingService.java
+++ src/main/java/bowling/BowlingService.java
@@ -5,41 +5,42 @@ import static java.util.Arrays.stream;
 public class BowlingService {
 
   public int computeScore(String pinsString) {
-
     int[] pins = parse(pinsString);
-    if (pins[0]==10) {
-      return  computeBasicScore(pins) +  strickBonusScore(pins);
+    return computeBasicScore(pins) + computeSpareBonus(pins) + computeStrikeBonus(pins);
+  }
+
+  private int computeStrikeBonus(int[] pins) {
+    for (int i = 0; i < pins.length - 2; i++) {
+      if (isStrike(pins[i])) {
+        return computeStrikeBonus(pins, i);
+      }
     }
-    return computeBasicScore(pins) + computeSpareBonus(pins);
+    return 0;
   }
 
-
-  private int strickBonusScore(int[] pins)
-  {
-    return pins[1] + pins[2];
+  private int computeStrikeBonus(int[] pins, int i) {
+    return pins[i + 1] + pins[i + 2];
   }
 
+  private boolean isStrike(int pin) {
+    return pin == 10;
+  }
 
   private int computeSpareBonus(int[] pins) {
     int bonus = 0;
-    for (int i = 2; i < pins.length; i+= 2) {
+    for (int i = 2; i < pins.length; i += 2) {
       bonus += computePreviousFrameSpareBonus(pins, i);
     }
     return bonus;
   }
 
-
-  private int computePreviousFrameSpareBonus(int[] pins, int i)
-  {
+  private int computePreviousFrameSpareBonus(int[] pins, int i) {
     return isPreviousFrameSpare(pins, i) ? pins[i] : 0;
   }
 
-
-  private boolean isPreviousFrameSpare(int[] pins, int i)
-  {
+  private boolean isPreviousFrameSpare(int[] pins, int i) {
     return pins[i - 2] + pins[i - 1] == 10;
   }
-
 
   private int[] parse(String pinsString) {
     return stream(pinsString.split(",")).mapToInt(Integer::parseInt).toArray();
