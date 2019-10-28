86f5540ef46c93de39af4a1f499c6aecf5b74a3f
obouallou <omar.bouallou@renault.com>
2019-10-28 11:10:55 +0100

    new approach with Frame object

--- src/main/java/bowling/BowlingService.java
+++ src/main/java/bowling/BowlingService.java
@@ -29,7 +29,6 @@ public class BowlingService {
     return bonus;
   }
 
-
   private int computeSpareBonus(int[] pins) {
     int bonus = 0;
     int pinIndex = 0;
@@ -51,12 +50,64 @@ public class BowlingService {
     return bonus;
   }
 
-
   private int[] parse(String pinsString) {
-    return stream(pinsString.split(",")).mapToInt(Integer::parseInt).toArray();
+    var pins = stream(pinsString.split(",")).mapToInt(Integer::parseInt).toArray();
+    int pinIndex = 0;
+    int frameIndex = 0;
+    BowlingFrame previousFrame = null;
+    while (frameIndex < 10) {
+      BowlingFrame frame;
+      if (pins[pinIndex] == 10) {
+        frame = new BowlingFrame(previousFrame, pins[pinIndex]);
+        frameIndex++;
+        pinIndex++;
+      } else if (pins[pinIndex] + pins[pinIndex + 1] == 10) {
+        if (frameIndex < 9) {
+          frame = new BowlingFrame(previousFrame, pins[pinIndex], pins[pinIndex + 1]);
+          pinIndex += 2;
+        } else {
+          frame =
+              new BowlingFrame(previousFrame, pins[pinIndex], pins[pinIndex + 1], pins[pinIndex + 2]);
+          pinIndex += 3;
+        }
+        frameIndex++;
+      } else {
+        frame = new BowlingFrame(previousFrame, pins[pinIndex], pins[pinIndex + 1]);
+        frameIndex++;
+        pinIndex += 2;
+      }
+      previousFrame = frame;
+    }
+    return null;
   }
 
   private int computeBasicScore(int[] pins) {
     return stream(pins).sum();
+  }
+
+  private class BowlingFrame {
+    private BowlingFrame previousFrame;
+    private int firstThrow;
+    private int secondThrow;
+    private int bonusThrow;
+
+    public BowlingFrame(BowlingFrame previousFrame, int firstThrow) {
+      this.previousFrame = previousFrame;
+      this.firstThrow = firstThrow;
+    }
+
+    public BowlingFrame(BowlingFrame previousFrame, int firstThrow,
+                        int secondThrow) {
+      this.previousFrame = previousFrame;
+      this.firstThrow = firstThrow;
+      this.secondThrow = secondThrow;
+    }
+
+    public BowlingFrame(BowlingFrame previousFrame, int firstThrow, int secondThrow, int bonusThrow) {
+      this.previousFrame = previousFrame;
+      this.firstThrow = firstThrow;
+      this.secondThrow = secondThrow;
+      this.bonusThrow = bonusThrow;
+    }
   }
 }
