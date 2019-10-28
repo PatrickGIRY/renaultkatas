112a18bedf75043fe7a2802700093368250f6649
obouallou <omar.bouallou@renault.com>
2019-10-28 12:07:03 +0100

    add an implementation for each Frame Type

--- src/main/java/bowling/BowlingService.java
+++ src/main/java/bowling/BowlingService.java
@@ -54,25 +54,33 @@ public class BowlingService {
     var pins = stream(pinsString.split(",")).mapToInt(Integer::parseInt).toArray();
     int pinIndex = 0;
     int frameIndex = 0;
+    BowlingFrame firstFrame;
+    if (pins[pinIndex] == 10) {
+      firstFrame = BowlingFrame.firstStrike();
+    } else if (pins[pinIndex] + pins[pinIndex +1] == 10){
+      firstFrame = BowlingFrame.firstSpare(pins[pinIndex]);
+    } else {
+      firstFrame = BowlingFrame.firstBasic(pins[pinIndex], pins[pinIndex+1] );
+    }
     BowlingFrame previousFrame = null;
+
     while (frameIndex < 10) {
       BowlingFrame frame;
       if (pins[pinIndex] == 10) {
-        frame = new BowlingFrame(previousFrame, pins[pinIndex]);
+        frame = BowlingFrame.strike();
         frameIndex++;
         pinIndex++;
       } else if (pins[pinIndex] + pins[pinIndex + 1] == 10) {
         if (frameIndex < 9) {
-          frame = new BowlingFrame(previousFrame, pins[pinIndex], pins[pinIndex + 1]);
+          frame = BowlingFrame.spare(pins[pinIndex]);
           pinIndex += 2;
         } else {
-          frame =
-              new BowlingFrame(previousFrame, pins[pinIndex], pins[pinIndex + 1], pins[pinIndex + 2]);
+          frame = BowlingFrame.lastSpare(pins[pinIndex], pins[pinIndex + 2]);
           pinIndex += 3;
         }
         frameIndex++;
       } else {
-        frame = new BowlingFrame(previousFrame, pins[pinIndex], pins[pinIndex + 1]);
+        frame = BowlingFrame.basic(pins[pinIndex], pins[pinIndex + 1]);
         frameIndex++;
         pinIndex += 2;
       }
@@ -85,29 +93,93 @@ public class BowlingService {
     return stream(pins).sum();
   }
 
-  private class BowlingFrame {
-    private BowlingFrame previousFrame;
-    private int firstThrow;
-    private int secondThrow;
-    private int bonusThrow;
+  private interface BowlingFrame {
 
-    public BowlingFrame(BowlingFrame previousFrame, int firstThrow) {
-      this.previousFrame = previousFrame;
-      this.firstThrow = firstThrow;
+    static BowlingFrame strike() {
+      return new StrikeFrame();
     }
 
-    public BowlingFrame(BowlingFrame previousFrame, int firstThrow,
-                        int secondThrow) {
-      this.previousFrame = previousFrame;
-      this.firstThrow = firstThrow;
-      this.secondThrow = secondThrow;
+    static BowlingFrame spare(int firstThrow) {
+      return new SpareFrame(firstThrow);
     }
 
-    public BowlingFrame(BowlingFrame previousFrame, int firstThrow, int secondThrow, int bonusThrow) {
-      this.previousFrame = previousFrame;
-      this.firstThrow = firstThrow;
-      this.secondThrow = secondThrow;
-      this.bonusThrow = bonusThrow;
+    static BowlingFrame lastSpare(int firstThrow, int bonusThrow) {
+      return new LastSpareFrame(firstThrow, bonusThrow);
+    }
+
+    static BowlingFrame basic(int firstThrow, int secondThrow) {
+      return new BasicFrame(firstThrow, secondThrow);
+    }
+
+
+    static BowlingFrame firstStrike()
+    {
+      return new FirstFrameStrike();
+    }
+
+
+    static BowlingFrame firstSpare(int firstThrow)
+    {
+      return new FirstFrameSpare(firstThrow);
+    }
+
+
+    static BowlingFrame firstBasic(int firstThrow, int secondThrow)
+    {
+      return new FirstFrameBasic(firstThrow, secondThrow);
+    }
+
+
+    class StrikeFrame implements BowlingFrame {}
+
+    class SpareFrame implements BowlingFrame {
+      private int firstThrow;
+
+      public SpareFrame(int firstThrow) {
+        this.firstThrow = firstThrow;
+      }
+    }
+
+    class LastSpareFrame implements BowlingFrame {
+      private int firstThrow;
+      private int bonusThrow;
+
+      public LastSpareFrame(int firstThrow, int bonusThrow) {
+        this.firstThrow = firstThrow;
+        this.bonusThrow = bonusThrow;
+      }
+    }
+
+    class BasicFrame implements BowlingFrame {
+      private int firstThrow;
+      private int secondThrow;
+
+      public BasicFrame(int firstThrow, int secondThrow) {
+        this.firstThrow = firstThrow;
+        this.secondThrow = secondThrow;
+      }
+    }
+
+
+    class FirstFrameStrike implements BowlingFrame {}
+
+    class FirstFrameSpare implements BowlingFrame {
+      private int firstThrow;
+
+
+      public FirstFrameSpare(int firstThrow) {
+        this.firstThrow = firstThrow;
+      }
+    }
+
+    class FirstFrameBasic implements BowlingFrame {
+      private int firstThrow;
+      private int secondThrow;
+
+      public FirstFrameBasic(int firstThrow, int secondThrow) {
+        this.firstThrow = firstThrow;
+        this.secondThrow = secondThrow;
+      }
     }
   }
 }
